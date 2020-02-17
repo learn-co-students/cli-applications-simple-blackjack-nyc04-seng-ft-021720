02fb2994fcd952f0fd6494ae609a6b647e9ad164
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-02-17 22:05:20 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -10,28 +10,46 @@ def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
 end
 
+def get_user_input
+  return gets.chomp
+end
+
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
+  get_user_input
 end
 
-def get_user_input
-  gets
-end
-
-def end_game
-  # code #end_game here 
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+  display_card_total(total = card1+card2)
+  total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  input = prompt_user
+  if input == 's'
+    # don't deal
+    total = card_total
+  elsif input == 'h'
+    # deal
+    new_card = deal_card
+    total = card_total + new_card
+  else
+    # bad input
+    invalid_command
+    return hit?(card_total)
+  end
+  return total
 end
 
 def invalid_command
   # code invalid_command here
+  puts
 end
 
 #####################################################
